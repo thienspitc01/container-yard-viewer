73da5235bc92379d8f8aca3546d453bc80c66c2c
thienspitc01 <thienspitc01@gmail.com>
2025-11-20 10:12:58 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,14 @@
-import { fileURLToPath, URL } from 'url';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          // FIX: Replace `__dirname` with ESM equivalent for path resolution.
-          '@': fileURLToPath(new URL('.', import.meta.url)),
-        }
-      }
-    };
-});
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['xlsx']
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
+})
